a9efbc6e118810500105705d1b854870cfe7b31e
Édouard Lopez <dev+git@edouard-lopez.com>
2016-09-27 17:31:13 +0200

    add test for __list_public_keys.fish
    require SSH directory

--- functions/__list_public_keys.fish
+++ functions/__list_public_keys.fish
@@ -1,4 +1,7 @@
 function __list_public_keys
+    if not set -q argv[1]
+        return 1
+    end
     set ssh_dir $argv[1]
 
     for key in $ssh_dir/*.pub
---
+++ test/list_public_keys.fish
@@ -0,0 +1,31 @@
+source $DIRNAME/../functions/__list_public_keys.fish
+set path $DIRNAME/$TESTNAME
+
+function setup
+    mkdir -p $path
+end
+
+
+
+test "$TEST - Fail if SSH directory argument is missing"
+    (
+        __list_public_keys
+        echo $status
+    ) = 1
+end
+
+test "$TEST - Silent if no public key in directory"
+    (
+        echo ( __list_public_keys $path | wc -l )
+    ) = 0
+end
+
+test "$TEST - List only *.pub files"
+    (
+        touch $path/key
+        touch $path/key1.pub $path/key2.pub
+        echo ( __list_public_keys $path/ | wc -l )
+        rm $path/key*
+    ) = 2
+end
+
